1ab0c52cf4f31a3ace350b95d51d83313ed6115e
Divyanshu422 <Divyanshu422@gmail.com>
2024-05-15 04:57:24 +0530

    Comment added

--- 1. NamasteReact/Lecture 6/src/components/Body.jsx
+++ 1. NamasteReact/Lecture 6/src/components/Body.jsx
@@ -8,16 +8,23 @@ import { useState, useEffect } from 'react'
 
 const Body = () =>{
 
-    const [data, setData] = useState([])
+    const [data, setData] = useState([]);
 
     useEffect(()=>{
         // Calling the function for fetching data
         fetchData();
     },[]);
 
-   const fetchData = async () => {
-    // Fetching the data from the API
-        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5489889&lng=77.2887429&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    // async function fetchData() {
+        const fetchData = async () =>{
+    // Fetching the data from the API`
+        /*
+          Since i am fetching the data from the swiggy server -> there is Possibility that
+          we get an error due to fetch from cross platform. as a result, browser will not render
+          the data. hence to sove this issue we need to add an extension to the browser which
+          is: Allow cross and then start it.
+        */
+          const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5489889&lng=77.2887429&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         // Converting the data into JSON format
         const jsonData = await data.json();
         console.log(jsonData.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
