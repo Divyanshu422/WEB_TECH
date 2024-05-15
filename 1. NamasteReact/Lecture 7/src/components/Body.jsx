b39149589218e4f342a1305b672d628ebc461795
Divyanshu Tyagi <divyanshutyagi@Divyanshus-MacBook-Pro.local>
2024-05-15 12:46:19 +0530

    Lecture 7

--- 1. NamasteReact/Lecture 7/src/components/Body.jsx
+++ 1. NamasteReact/Lecture 7/src/components/Body.jsx
@@ -1,7 +1,6 @@
-// Creating the body component:
+
 
 import RestaurantCard from './RestaurantCard'
-// import resList from '../utils/MockData' . =>Since we are making an api call and setting the data to the State variable on which the Component is mapped so we no longer need it
 import { useState, useEffect } from 'react'
 import Shimmer from './Shimmer';
 
@@ -11,23 +10,13 @@ const Body = () =>{
     const [filterData, setFilterData] = useState([]);
 
     useEffect(()=>{
-        // Calling the function for fetching data
         fetchData();
     },[]);
 
-    // async function fetchData() {
-        const fetchData = async () =>{
-    // Fetching the data from the API`
-        /*
-          Since i am fetching the data from the swiggy server -> there is Possibility that
-          we get an error due to fetch from cross platform. as a result, browser will not render
-          the data. hence to sove this issue we need to add an extension to the browser which
-          is: Allow cross and then start it.
-        */
-          const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5489889&lng=77.2887429&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
-        // Converting the data into JSON format
+
+    const fetchData = async () =>{
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5489889&lng=77.2887429&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         const jsonData = await data.json();
-            //! Using optional chaining
         setData(jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
         setFilterData(jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
    }
@@ -37,30 +26,16 @@ const Body = () =>{
     }
 
     function changeHandler (e){
-        // console.log(resList);
         const FilterList = data.filter((rest) => rest.info.avgRating>4)
         setData(FilterList);
     }
 
-
-    //  Loading Spinner => this is known as conditional rendering
-    // if (data.length == 0){
-    //     return <h1>Loading...</h1>;
-    // }
-
-    // Using Shimmer component: => implemented using terinary operator in js
-    // if (data.length == 0){
-    //         return <Shimmer/>;
-    //     }
-
-   
     function searchHandler(e){
-        // ! Includes method
         const FilterData = data.filter((filteredData =>(filteredData.info.name.toLowerCase().includes(searchText.toLowerCase()))));
         setFilterData(FilterData);      
     }
 
-    return data.length == 0? <Shimmer/> : (
+    return  filterData && filterData.length === 0? <Shimmer/> : (
         <div className="body">
             <div className='filter'>
                 <div className='search'>
--- 1. NamasteReact/Lecture 7/src/components/Header.jsx
+++ 1. NamasteReact/Lecture 7/src/components/Header.jsx
@@ -1,20 +1,11 @@
- 
  import {LOGO_URL} from '../utils/constants'
- 
- import { useState } from 'react';
- // Creating the header component: has 2 things image and links
- const Header = () =>{
-    //  Creating a local variable to change the state of Button -> Login and logout
+ import { useState, useEffect } from 'react';
 
+ const Header = () =>{
     const [isLoggedIn, setLoggedIn] = useState('Login');
 
     function clickHandler(e){
-        if (isLoggedIn == 'Login'){
-            setLoggedIn('LogOut');
-        }
-        else{
-            setLoggedIn('Login');
-        }
+        isLoggedIn == 'Login'? setLoggedIn('LogOut') : setLoggedIn('Login');
     }
 
     return(
@@ -30,7 +21,6 @@
                     <li>About Us</li>
                     <li>Contact US</li>
                     <li>Cart</li>
-                   {/* <button className='login' onClick={() => (setLoggedIn('LogOut')) }>{isLoggedIn}</button> */}
                    <button className ='login' onClick={clickHandler}>{isLoggedIn}</button>
                 </ul>
             </div>
