8d71fd3dd2bb0cd5127e76d46f13265232b18b06
Divyanshu Tyagi <divyanshutyagi@Divyanshus-MacBook-Pro.local>
2024-05-16 15:12:13 +0530

    Lecture 7

--- 1. NamasteReact/Lecture 7/src/components/RestaurantMenu.jsx
+++ 1. NamasteReact/Lecture 7/src/components/RestaurantMenu.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import {useState, useEffect } from 'react'
 import Shimmer from './Shimmer';
+import { useParams } from 'react-router-dom';
+import { MenuApi } from '../utils/constants';
 
 function RestaurantMenu() {
 
   const [resData, setResData] = useState([]); 
   const [menuList, setMenuList] = useState([]);
+  const {resId} = useParams();
+
 
   //  To make a api call we will use useEffect
   useEffect(() => {
@@ -14,7 +18,8 @@ function RestaurantMenu() {
 
   //  making an api call 
   const fetchMenu = async () => {
-    const data = await fetch('https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.61426970867492&lng=77.3554216325283&restaurantId=883912&catalog_qa=undefined&submitAction=ENTER');
+
+    const data = await fetch(MenuApi});
     const menu = await data.json();
     console.log(menu?.data?.cards[4].groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards);
       setResData(menu?.data?.cards[2]?.card?.card?.info);
@@ -28,8 +33,8 @@ function RestaurantMenu() {
         <h2>Menu</h2>
         <ul>
   {
-    menuList.map((menu) => (
-      <li className='res-card'>
+    menuList.map((menu,index) => (
+      <li key={index} className='res-card'>
         {menu.card.info.name}
         <img className='res-logo' src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/${menu.card.info.imageId}`} />
       </li>
