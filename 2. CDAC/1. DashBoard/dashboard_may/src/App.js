11bf967449844ff6eefcbd93ae7cf2d3ba67c3d8
Divyanshu Tyagi <divyanshutyagi@Divyanshus-MacBook-Pro.local>
2024-05-17 12:40:09 +0530

    Draft1

--- 2. CDAC/1. DashBoard/dashboard_may/src/App.js
+++ 2. CDAC/1. DashBoard/dashboard_may/src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/Home";
+import { createBrowserRouter,   RouterProvider, Outlet } from "react-router-dom";
+import User from "./pages/User";
+import Product from "./pages/Product";
+import Navbar from "./components/Navbar";
+import Menu from "./components/Menu";
+import Footer from "./components/Footer";
+import Login from "./pages/Login";
 
 function App() {
+
+  //  Creating the detailed layout: which includes the header, sideBar (Developer is mentioning the sideBar as Menu) and footer
+
+  const Layout = () =>{
+    return (
+      <div className="">
+        <Navbar/>
+        <div>
+          <div>
+            <Menu/>
+          </div>
+          <div>
+            <Outlet/>
+          </div>
+        </div>
+        <Footer/>
+      </div>
+    )
+  }
+
+
+
+
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout/>,
+      children:[
+        {
+          path: "/home",
+          element: <Home/>
+        },
+        {
+          path: "/users",
+          element: <User/>
+        },
+        {
+          path: "/products",
+          element: <Product/>
+        }
+      ] 
+    },
+    {
+      path: '/login',
+      element:<Login/>
+    }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
