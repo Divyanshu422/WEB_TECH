2b2afb3cfcfc1e8b349850e1e90e5280eba041b1
Divyanshu Tyagi <divyanshutyagi@Divyanshus-MacBook-Pro.local>
2024-05-20 12:28:01 +0530

    Lecture 9

---
+++ 1. NamasteReact/Lecture 9/ReadmeFile/ProjectDetail.md
@@ -0,0 +1,18 @@
+# Lecture 2 commands used in the chapter
+
+1. Step 1: initialize `npm` into your repo ==> using `npm init -y`.
+2. Step 2: Install the parecel => `npm install -D parcel`
+3. Step 3: install the gitignore file: `.gitignore`
+4. Step 4: Install the react and react-dom libraries: `npm install react-dom` and `npm install react`
+5. Step 5: Link the app.js file into the index.html file.
+   `<script type="module" src="App.js"></script>`
+
+6. Create the root element in the html file: `<div id="root"></div>`
+
+7. import the react and react-dom libraries into the app.js file
+
+# lecture 7:
+
+1. install the dependency for routing `npm i react-router-dom`
+
+# lecture 8: Class Based component
---
+++ 1. NamasteReact/Lecture 9/src/components/Body.jsx
@@ -0,0 +1,75 @@
+
+
+import RestaurantCard from './RestaurantCard'
+import { useState, useEffect } from 'react'
+import Shimmer from './Shimmer';
+
+const Body = () =>{
+    const [searchText, setSearchText] = useState(''); 
+    const [data, setData] = useState([]);
+    const [filterData, setFilterData] = useState([]);
+
+    useEffect(()=>{
+        fetchData();
+    },[]);
+
+
+    const fetchData = async () =>{
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5489889&lng=77.2887429&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const jsonData = await data.json();
+        setData(jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setFilterData(jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+   }
+    
+    const style = {
+        padding: '10px',
+    }
+
+    function changeHandler (e){
+        const FilterList = data.filter((rest) => rest.info.avgRating>4)
+        setData(FilterList);
+    }
+
+    function searchHandler(e){
+        const FilterData = data.filter((filteredData =>(filteredData.info.name.toLowerCase().includes(searchText.toLowerCase()))));
+        setFilterData(FilterData);      
+    }
+
+    return  filterData && filterData.length === 0? <Shimmer/> : (
+        <div className="body">
+            <div className='filter'>
+                <div className='search'>
+                    <input type="text" className="search-box" value ={searchText} onChange = {(e)=> (setSearchText(e.target.value))}/>
+                    <button className="search-btn" onClick ={searchHandler}>Search</button>
+                </div>
+                <div>
+                    <button className="filter-btn" onClick={changeHandler} >Top rated Restaurant</button>
+                </div>
+            </div>
+            <div className="res-container">
+                {
+                    filterData.map((data,index) => (
+                   
+                            <RestaurantCard key={index} resData = {data.info} />
+                        
+                    ))
+                }
+            </div>
+        </div>
+    )
+
+} 
+export default Body;
+
+
+
+
+
+
+
+
+
+
+
+
+ 
