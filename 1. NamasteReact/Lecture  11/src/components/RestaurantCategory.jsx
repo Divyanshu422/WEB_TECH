7484b1bced036d8fe04eb0571ba81a4e5018add2
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-05-22 16:02:46 +0530

    Lecture 11

--- 1. NamasteReact/Lecture  11/src/components/RestaurantCategory.jsx
+++ 1. NamasteReact/Lecture  11/src/components/RestaurantCategory.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { MdArrowDropDownCircle } from "react-icons/md";
 import ItemList from './ItemList';
 
-function RestaurantCategory({data}) {
-    // console.log(data);
+function RestaurantCategory({ data }) {
+    const [show, setShow] = useState(false);
 
-  return (
-    <div className='w-6/12 mx-auto my-4 shadow-lg rounded-md'> 
-        {/* Creating the according header */}
-        <div className='flex justify-between mx-4'>
-            <span className='font-bold text-2xl text-left px-2 py-2 '>{data?.title} ({data?.itemCards.length})</span>
-                    {/* <span>{data?.itemCards.length}</span> */}
+    const handleclick = () => {
+        setShow(!show);
+    };
 
-            <span > <MdArrowDropDownCircle size={20} /> </span>
+    return (
+        <div className='w-6/12 mx-auto my-4 shadow-lg rounded-md'> 
+            <div className='flex justify-between mx-4'>
+                <span className='font-bold text-2xl text-left px-2 py-2 '>{data?.title} ({data?.itemCards.length})</span>
+                <span onClick={handleclick} className='hover:cursor-pointer'> <MdArrowDropDownCircle className='w-6 h-6'/> </span>
+            </div>
+            <div className={`overflow-hidden transition-all duration-500 ease-in-out ${show ? 'max-h-screen' : 'max-h-0'}`}>
+                {show && <ItemList data={data.itemCards} />}
+            </div>
         </div>
-        {/* Item List */}
-        <ItemList data = {data.itemCards}/>
-    </div>
-  )
+    );
 }
 
-export default RestaurantCategory
+export default RestaurantCategory;
