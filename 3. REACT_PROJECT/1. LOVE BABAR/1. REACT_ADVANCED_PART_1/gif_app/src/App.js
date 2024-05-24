cae7894908b63a54e5de702cfa4719de0889763b
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-05-25 00:47:43 +0530

    Developed the 2nd Gif in which user can pass the input and based on that corresponding gif is created

--- 3. REACT_PROJECT/1. LOVE BABAR/1. REACT_ADVANCED_PART_1/gif_app/src/App.js
+++ 3. REACT_PROJECT/1. LOVE BABAR/1. REACT_ADVANCED_PART_1/gif_app/src/App.js
@@ -2,11 +2,11 @@ import Random from "./components/Random";
 import Tags from "./components/Tags";
 function App() {
   return (
-   <div className = "flex flex-col background h-screen w-screen items-center">
+   <div className = "flex flex-col background w-screen items-center">
       {/* Top level heading */}
       <h1 className ="bg-white text-center py-2 w-10/12 mx-auto
       text-3xl font-bold rounded-xl my-10 shadow-sm">Random GIFS</h1>
-      <div className="flex w-screen">
+      <div className="flex flex-col gap-y-10 w-screen">
         <Random />
         <Tags />
       </div>
--- 3. REACT_PROJECT/1. LOVE BABAR/1. REACT_ADVANCED_PART_1/gif_app/src/components/Tags.js
+++ 3. REACT_PROJECT/1. LOVE BABAR/1. REACT_ADVANCED_PART_1/gif_app/src/components/Tags.js
@@ -1,8 +1,45 @@
 import React from 'react'
+import { useState, useEffect } from'react'
+import axios from 'axios';
+import Spinner from '../spinner/Spinner.js';
 
+const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 function Tags() {
+    const [gif, setGif] = useState('');
+    const [loading, setLoading] = useState('false');
+    const [tag, setTag] = useState('');
+    
+
+    //* Making the ApI in the useEffect:
+    useEffect(()=>{
+        fetchData();
+    },[])
+
+    async function fetchData() {
+        setLoading(true);
+        const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tag}`;
+        const response = await axios.get(url);
+        console.log(response?.data?.data);
+        setGif(response?.data?.data?.images?.downsized?.url);
+        setLoading(false)
+    }
+   
+
+    function gifHandler() {
+        fetchData()
+    }
+
+    function changeHandler(event){
+      setTag(event.target.value)
+    }
   return (
-    <div>
+    <div className='flex flex-col bg-blue-500 border-[1px] border-black py-10 w-8/12 mx-auto items-center rounded-2xl gap-y-10'>
+        <h1 className='text-2xl font-bold text-center uppercase'> Random Gif </h1>
+        {
+        (loading)? (<Spinner/>):(<img src={gif} alt="" className='w-[450px] h-[350px]'/>)
+        }
+        <input type = 'text' className='w-8/12  text-xl text-center mx-auto py-2 rounded-lg' value ={tag} onChange={changeHandler}/>
+        <button className='bg-green-200 text-xl text-center w-8/12 mx-auto py-2 rounded-lg hover:scale-105 hover:bg-slate-500' onClick={gifHandler}> Get Random </button>
       
     </div>
   )
