4396dc1f49506eb04b3a5fb7e17eb9524542108f
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-05-28 12:40:55 +0530

    Fetch data from api in Home.js file

--- 3. REACT_PROJECT/1. LOVE BABAR/2. REACT ADVANCED PART-2/redux_website/src/pages/Home.js
+++ 3. REACT_PROJECT/1. LOVE BABAR/2. REACT ADVANCED PART-2/redux_website/src/pages/Home.js
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import { useEffect } from 'react';
 function Home() {
+  const API_URL = "https://fakestoreapi.com/products";
+
+  useEffect(()=>{
+    fetchProductData();
+  },[])
+  async function fetchProductData(){
+    const data = await fetch(API_URL);
+    const products = await data.json();
+    console.log(products);
+  }
   return (
-    <div>Home</div>
+    <div>
+      this is home page with cards
+    </div>
   )
 }
 
