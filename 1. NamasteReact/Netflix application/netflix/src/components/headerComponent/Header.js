da776363f5ec8e809adfebc34719a8fe69544ab7
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-06-02 17:32:33 +0530

    Header completed but styling not matched

---
+++ 1. NamasteReact/Netflix application/netflix/src/components/headerComponent/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className=' flex items-center justify-around absolute w-screen bg-gradient-to-b from-black  '>
+
+        <img src =" https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png " alt="Netflix Logo" className ="w-60" />
+
+        <div className=' flex gap-x-10 '>
+            <div className=' border-[1px] py-[0.5rem] px-4 border-white rounded-sm text-white'>
+                <select id="language" name="language" className='bg-inherit'>
+                    <option lang="en" label="English" value="en-IN" selected="">English</option>
+                    <option lang='hi' value="hindi">हिन्दी</option>
+                </select>
+            </div>
+            <button className='bg-red-600 py-1 px-4 text-white font-medium rounded-sm'>Sign Out</button>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Header
--- 1. NamasteReact/Netflix application/netflix/src/components/loginComponent/Login.js
+++ 1. NamasteReact/Netflix application/netflix/src/components/loginComponent/Login.js
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import Header from '../headerComponent/Header'
 const Login = () => {
   return (
-    <div>
-    Login
+    <div className=''>
+      <Header/>
+      <img src ="https://assets.nflxext.com/ffe/siteui/vlv3/dd4dfce3-1a39-4b1a-8e19-b7242da17e68/86742114-c001-4800-a127-c9c89ca7bbe4/IN-en-20240527-popsignuptwoweeks-perspective_alpha_website_small.jpg" alt='background' className='w-screen h-screen' />
+
+      
     </div>
   )
 }
