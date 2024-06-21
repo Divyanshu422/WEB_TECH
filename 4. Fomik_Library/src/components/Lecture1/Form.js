5c0fe86be934b0d83f769219adcf7881d1e2c412
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-06-21 18:18:15 +0530

    Handling the submission on form using 2 step: onSubmit handler and onSubmit property in form tag and useFormik Hook

--- 4. Fomik_Library/src/components/Lecture1/Form.js
+++ 4. Fomik_Library/src/components/Lecture1/Form.js
@@ -1,26 +1,20 @@
 /*
-    * Lecture 3: Managing the state
-    * In the youtube form we have 3 field => name, email, channel. To track these 3 
-    * fields we need the state variable for that. But in the case of formik => no need 
-    * to create the state variable rather it is useFormik() which return the object
-    * will track the state.
+    * Handling the submission
+    * There are 2 step to handle the submission using formik:
+    *       1. specify the onSubmit handler on the form tag whose value is equal to 
+    *               formik.handleSubmit (formik is the object returned by hook)
+    *       2. Define the onSubmit property in the useFormik hook. the property 
+    *           calls the callBack function. this callBack function receive 'values'
+    *           property as the parameter. values stores the current state value of 
+    *           each field.
+    *           the values incorporate the all values of field which we have accessed
+    *           using formik.values
+    *       3. when we click on the submit button => formik will automatically execute
+    *           the onSubmit property defined inside the useHook
 */
-/*
-    * Step 1: as discussed the useFormik hook consume the object. so the first property
-    * we need to pass is the initialValues (Camel_Case). the initialValue property is 
-    * an object which takes the initial values for all the form field => name, email, 
-    * channel. 
-    *   Note: the property for initial value corresponds to the name attribute to the
-    *         Channel.
-    */
 
-    /*
-        * we need to add the onChange and values prop for each of the field property.
-        * onChange={formik.handleChange}. handleChange is the method give by hook.
-        * value = {formik.values.name}. values is property provided by the hook
-    */
 import React from 'react'
-import { useFormik } from 'formik'      //! Import the formik hook
+import { useFormik } from 'formik'    
 
 
 function Form() {
@@ -31,21 +25,24 @@ function Form() {
             name:'',
             email:'',
             channel:''
-           } 
+           }, 
+           // onSubmit handler
+           onSubmit: (values) => {
+                console.log('checking the values', values);
+           }
     })
 
-    console.log(formik.values);
 
   return (
     <div>
-        <form>
+        <form onSubmit={formik.handleSubmit}>
             <label htmlfor='name'>Name:</label>
             <input type='text' id ='name' name='name' onChange={formik.handleChange} value = {formik.values.name}/>
             <label htmlfhtmlor='email'>Email:</label>
             <input type='email' id ='email' name='email' onChange={formik.handleChange} value = {formik.values.email}/>
             <label htmlfor='channel'>Channel:</label>
             <input type='text' id ='channel' name='channel' onChange={formik.handleChange} value = {formik.values.channel}/>
-            <button>Submit</button>
+            <button type = 'submit'>Submit</button>
         </form>
     </div>
   )
