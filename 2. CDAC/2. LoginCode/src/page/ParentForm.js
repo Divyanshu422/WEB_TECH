e0208b8440fa8bc86ee1593850fa00b884900872
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-06-28 13:00:33 +0530

    Created the 2 button and implemented their handle function => check the console"

--- 2. CDAC/2. LoginCode/src/page/ParentForm.js
+++ 2. CDAC/2. LoginCode/src/page/ParentForm.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function ParentForm() {
-    
+
+    //! Step 1: Creating the state variable which is used to track the page in the form
+    const [page, setPage] = useState(0);
+
+    //! Step 2: Creating the list which is displayed on the UI reflecting which page am i.
+    const formList = ["Personal Details", "Education", "Experience"];
+    const formLength = formList.length;
+
+    //* Creating the 2 handle function for the back and forward click on the button tag
+    const handleNext = () => {
+        setPage(page === formLength -1 ? 0 : page+1)
+    }
+    const handlePrev = () => {
+        setPage(page === 0 ? formLength - 1 : page - 1)
+    }
+
+    console.log(page);
   return (
-    <div>ParentForm</div>
+    <div>
+
+        {/* Button */}
+        <button onClick={handlePrev}>Previous</button>
+        <button onClick={handleNext}>Next</button>
+
+    </div>
   )
 }
 
