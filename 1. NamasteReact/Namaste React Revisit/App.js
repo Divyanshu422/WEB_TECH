9d7a4127f9f2afb7f2fbbd891a3fb0e2a07a26d7
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-07-01 14:35:38 +0530

    Creating the complex Structure in the react

--- 1. NamasteReact/Namaste React Revisit/App.js
+++ 1. NamasteReact/Namaste React Revisit/App.js
@@ -1,10 +1,20 @@
+    /*
+        * Creating the nested structure
+        *     <div id = 'parent'>
+        *           <div id = 'child'>
+        *               <h1> Hello World! </h1>
+        *           </div>
+        *     <div>
+    */
+    
+    
+    
+    const heading = React.createElement("div", {id:'parent'}, 
+                                React.createElement("div", {id:'child'},
+                                        React.createElement("h1", {}, "Hello World!")));
 
-    const heading = React.createElement("h1", {id:'heading'}, "Hello world from React");
 
-        //  what is heading? or what createElement gives to developer? => an object or an element
-        // Checking the type of heading
-        console.log(heading); //! An object which has inbuilts props (like id, attribute etc)
-
+    console.log(heading);
     const root = ReactDOM.createRoot(document.getElementById("root"));
 
     root.render(heading); // the meaning of this line is to take h1 element created in react and place in the 
