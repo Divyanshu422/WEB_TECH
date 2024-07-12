b819183608216d732a770bf4d5331aeb249384ae
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-07-12 12:59:42 +0530

    Creating the tableInstance which cntains the method and properties which is used in the comoponenet

--- 2. CDAC/3. ReactTable/src/components/BasicTable.js
+++ 2. CDAC/3. ReactTable/src/components/BasicTable.js
@@ -5,27 +5,16 @@ import { Columns } from './Column'
 
 export const BasicTable = () => {
 
-        //*  the useTable hook recommends to memorize the row and columns using useMemo hook
-        /*
-            * The useMemo hook is a React hook that helps optimize performance by memoizing, or caching, 
-            * expensive calculations. It takes a callback function that performs a calculation and an array of 
-            * dependencies. If any of the dependencies change between renders, useMemo will re-run the 
-            * calculation and return the new result. If none of the dependencies change, useMemo will return the 
-            * memoized result from the previous render, avoiding the expensive re-calculation.
-        */
-
         const column = useMemo(()=>{
             return Columns
         },[])
         const data = useMemo(()=> MOCK_DATA, [])
 
-        //* Passing the output of useMemo hook to the useTable
-        useTable({
+        //* 1. The call to the useTable return an table instance which is stored in the constant variable
+        const tableInstance = useTable({
             columns: column,
             data: data
         })
-
-
 return (
         <div>
                 
