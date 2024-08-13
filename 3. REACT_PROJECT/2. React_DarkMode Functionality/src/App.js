7e63d6d1f388c0b43128641ae82282065d2cc74f
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-08-13 18:09:14 +0530

    themeVariable created and passed to the NavBar as prop which is used to decide whether it is dark or light

--- 3. REACT_PROJECT/2. React_DarkMode Functionality/src/App.js
+++ 3. REACT_PROJECT/2. React_DarkMode Functionality/src/App.js
@@ -1,8 +1,13 @@
+import { useState } from "react";
 import NavBar from "./components/NavBar";
 function App() {
+
+  // Creating the state variable which is passed to the NavBar for checking the dark mode functionality
+  const[theme, setTheme] = useState('light')
   return (
     <div>
-      <NavBar/>
+      {/* Passing the theme variable and function */}
+      <NavBar theme= {theme} setTheme= {setTheme}/>
     </div>
   );
 }
