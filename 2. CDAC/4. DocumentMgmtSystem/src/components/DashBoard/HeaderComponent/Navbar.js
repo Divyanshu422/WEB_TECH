1fd7a39f254220ec5b5f733e08eff01137659065
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-08-16 11:15:05 +0530

    Logo added but dimension not fixed

--- 2. CDAC/4. DocumentMgmtSystem/src/components/DashBoard/HeaderComponent/Navbar.js
+++ 2. CDAC/4. DocumentMgmtSystem/src/components/DashBoard/HeaderComponent/Navbar.js
@@ -1,9 +1,69 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "../../../context/ThemeContext";
+import Logo from "../../../Assets/Navbar/Logo.png";
 function Navbar() {
   const { theme, setTheme } = useContext(ThemeContext);
   console.log(theme);
-  return <div>CHECKING THE ThemeContex</div>;
+
+  // Creating the toggle function for changing the theme and setting the value
+  const toggle_mode = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+  return (
+    <div
+      className={` w-full ${
+        theme === "light" ? "bg-[#ced8ff]" : "bg-[#222]"
+      } transition-all duration-5000 ease-in-out`}
+    >
+      <div
+        className={`w-100 flex items-center justify-between ${
+          theme === "light" ? "bg-white" : "bg-gray-800"
+        } px-14 transition-all duration-5000 ease-in-out`}
+      >
+        {/* Logo */}
+        <img src={Logo} alt="Logo" className="w-20 cursor-pointer" />
+
+        {/* <ul
+          className={`flex gap-10 cursor-pointer font-bold text-xl ${
+            theme === "light" ? "text-black" : "text-white"
+          }`}
+        >
+          <li>Home</li>
+          <li>Products</li>
+          <li>Features</li>
+          <li>About</li>
+        </ul> */}
+
+        {/* Search Box */}
+        {/* <div
+          className={`flex items-center ${
+            theme === "light" ? "bg-gray-200" : "bg-gray-700"
+          } px-[10px] gap-3 py-[10px] rounded-[50px] transition-all duration-5000 ease-in-out`}
+        >
+          <input
+            type="text"
+            placeholder="Search"
+            className={`outline-none bg-transparent font-[18px] border-[0] ml-3 max-w-[200px] ${
+              theme === "light" ? "text-black" : "text-white"
+            }`}
+          />
+          <img
+            src={theme === "light" ? search_icon_dark : search_icon_light}
+            alt="Search Icon"
+            className="w-[20px] mr-2 cursor-pointer"
+          />
+        </div> */}
+
+        {/* Toggle icon */}
+        {/* <img
+          src={theme === "light" ? toggle_night : toggle_light}
+          alt="Toggle Theme"
+          className="w-[50px] cursor-pointer ml-[40px]"
+          onClick={toggle_mode}
+        /> */}
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
--- 2. CDAC/4. DocumentMgmtSystem/src/components/DashBoard/SideBarComponent/SideBar.js
+++ 2. CDAC/4. DocumentMgmtSystem/src/components/DashBoard/SideBarComponent/SideBar.js
@@ -1,36 +1,43 @@
-import React from 'react'
-import { Columns3, ChevronFirst, ChevronLast, MoreVertical } from 'lucide-react';
-import SideBarItems from './SideBarItems'
+import React from "react";
+import {
+  Columns3,
+  ChevronFirst,
+  ChevronLast,
+  MoreVertical,
+} from "lucide-react";
+import SideBarItems from "./SideBarItems";
 
 //  Importing the image of user and Logo From Assets Folder
-import FileManangementLogo from '../../../Assets/FileManangementLogo.png' 
-import User_Image from '../../../Assets/User_Image.png'
-import { useContext } from 'react';
-import { SideBarToggleContext } from '../../../context/SideBarToggleContext';
+// import FileManangementLogo from '../../../Assets/FileManangementLogo.png'
+import User_Image from "../../../Assets/User_Image.png";
+import { useContext } from "react";
+import { SideBarToggleContext } from "../../../context/SideBarToggleContext";
 
-function SideBar({children}) {
+function SideBar({ children }) {
+  const { expanded, setExpanded } = useContext(SideBarToggleContext);
 
-  const {expanded, setExpanded} = useContext(SideBarToggleContext);
-
-  return (  
+  return (
     // Aside => symmentic tag
-    <aside className='h-screen  '>
+    <aside className="h-screen  ">
       <nav className=" h-full bg-gradient-to-b from-blue-50  to-indigo-100 flex flex-col bg-white border-2 shadow-sm">
-
         {/* Added the SideBar Component  */}
-        <div className='p-4 pb-2 flex justify-between items-center'>
-          <button className='p-1.5 rounded-lg bg-gray-300 hover:bg-slate-600' onClick ={()=>setExpanded((currentStateOfExpanded)=>!currentStateOfExpanded)}>
-            {expanded? <Columns3 size={30} />: <Columns3 size={30} />}
+        <div className="p-4 pb-2 flex justify-between items-center">
+          <button
+            className="p-1.5 rounded-lg bg-gray-300 hover:bg-slate-600"
+            onClick={() =>
+              setExpanded((currentStateOfExpanded) => !currentStateOfExpanded)
+            }
+          >
+            {expanded ? <Columns3 size={30} /> : <Columns3 size={30} />}
           </button>
         </div>
         {/*  Adding the elements of the SideBar */}
-        <ul className='flex-1 px-3'>
-          <SideBarItems/>
+        <ul className="flex-1 px-3">
+          <SideBarItems />
         </ul>
-
       </nav>
     </aside>
-  )
+  );
 }
 
-export default SideBar
+export default SideBar;
